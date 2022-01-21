3fb2f060fc37267cb7b2262f97bf393dcbcd0b0c
Matheus Teixeira <mathbteixeira@gmail.com>
2022-01-20 22:49:54 -0200

    Conhecendo métodos de List - pt. 2

--- src/com/dio/collections/ExemploList.java
+++ src/com/dio/collections/ExemploList.java
@@ -1,8 +1,6 @@
 package src.com.dio.collections;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class ExemploList {
     public static void main(String[] args) {
@@ -50,6 +48,44 @@ public class ExemploList {
         System.out.println("Exiba todas as notas na ordem em que foram informados: ");
         for (Double nota : notas) System.out.println(nota);
 
-        
+        System.out.println("Exiba a terceira nota adicionada: " + notas.get(2));
+        System.out.println(notas.toString());
+
+        System.out.println("Exiba a menor nota: " + Collections.min(notas));
+
+        System.out.println("Exiba a maior nota: " + Collections.max(notas));
+
+        Iterator<Double> iterator = notas.iterator();
+        Double soma = 0d;
+        while(iterator.hasNext()){
+            Double next = iterator.next();
+            soma += next;
+        }
+        System.out.println("Exiba a soma dos valores: " + soma);
+
+        System.out.println("Exiba a média das notas: " + (soma/notas.size()));
+
+        System.out.println("Remova a nota 0: ");
+        notas.remove(0d);
+        System.out.println(notas);
+
+        System.out.println("Remova a nota da posição 0");
+        notas.remove(0);
+        System.out.println(notas);
+
+        System.out.println("Remova as notas menores que 7 e exiba a lista: ");
+        Iterator<Double> iterator1 = notas.iterator();
+        while(iterator1.hasNext()) {
+            Double next = iterator1.next();
+            if(next < 7) iterator1.remove();
+        }
+        System.out.println(notas);
+
+        /*System.out.println("Apague toda a lista");
+        notas.clear();
+        System.out.println(notas);
+        */
+        System.out.println("Confira se a lista está vazia: " + notas.isEmpty());
+
     }
 }
