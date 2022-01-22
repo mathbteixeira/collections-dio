0048886048ff749a55f8bed5a5e54de4683929b9
Matheus Teixeira <mathbteixeira@gmail.com>
2022-01-22 17:07:06 -0200

    Ordenação de elementos em uma coleção Set

---
+++ src/com/dio/collections/sets/ExemploOrdenacaoSet.java
@@ -0,0 +1,65 @@
+package src.com.dio.collections.sets;
+
+import java.util.*;
+
+/*Dadas as seguintes informações sobre minhas séries favoritas,
+crie um conjunto e ordene este conjunto exibindo:
+(nome - genero - tempo de episódio);
+Série 1 = Nome: got, genero: fantasia, tempoEpisodio: 60
+Série 2 = nome: dark, genero: drama, tempoEpisodio: 60
+Série 3 = nome: that '70s show, genero: comédia, tempoEpisodio: 25
+*/
+
+public class ExemploOrdenacaoSet {
+    public static void main(String[] args) {
+        System.out.println("--\tOrdem aleatória\t--"); // Ordem aleatória = HashSet
+        Set<Serie> minhasSeries = new HashSet<>(){{
+            add(new Serie("got", "fantasia", 60));
+            add(new Serie("dark", "drama", 60));
+            add(new Serie("that '70s show", "comédia", 25));
+        }};
+        for (Serie serie: minhasSeries) {
+            System.out.println(serie.getNome() + " - "
+                    + serie.getGenero() + " - " + serie.getTempoEpisodio());
+        }
+
+        System.out.println("--\tOrdem inserção\t--"); // Ordem inserção = LinkedHashSet
+        Set<Serie> minhasSeries1 = new LinkedHashSet<>() {{
+            add(new Serie("got", "fantasia", 60));
+            add(new Serie("dark", "drama", 60));
+            add(new Serie("that '70s show", "comédia", 25));
+        }};
+        for (Serie serie: minhasSeries1) {
+            System.out.println(serie.getNome() + " - "
+                    + serie.getGenero() + " - " + serie.getTempoEpisodio());
+        }
+
+        System.out.println("--\tOrdem natural (TempoEpisodio)\t--"); // Ordem natural = TreeSet
+        Set<Serie> minhasSeries2 = new TreeSet<>(minhasSeries1);
+        for (Serie serie: minhasSeries2) {
+            System.out.println(serie.getNome() + " - "
+                    + serie.getGenero() + " - " + serie.getTempoEpisodio());
+        }
+
+        System.out.println("--\tOrdem Nome/Gênero/TempoEpisodio\t--"); // Ordem customizada (Comparator) = TreeSet
+        Set<Serie> minhasSeries3 = new TreeSet<>(new ComparatorNomeGeneroTempoEpisodio());
+        minhasSeries3.addAll(minhasSeries);
+        for (Serie serie: minhasSeries3) {
+            System.out.println(serie.getNome() + " - "
+                    + serie.getGenero() + " - " + serie.getTempoEpisodio());
+        }
+
+        //Pra você
+        /*
+            System.out.println("--\tOrdem gênero\t--");
+            System.out.println("--\tOrdem Tempo Episódio\t--");
+        */
+        System.out.println("--\tOrdem gênero\t--");
+        Set<Serie> minhasSeries4 = new TreeSet<>(new ComparatorGenero());
+        minhasSeries4.addAll(minhasSeries);
+        for (Serie serie: minhasSeries4) {
+            System.out.println(serie.getNome() + " - "
+                    + serie.getGenero() + " - " + serie.getTempoEpisodio());
+        }
+    }
+}
