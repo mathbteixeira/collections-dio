d325a9e472c2eba76d306e1dc8caa9905d038e8f
Matheus Teixeira <mathbteixeira@gmail.com>
2022-01-23 15:21:47 -0200

    Set - Exercicio Proposto 01

---
+++ src/com/dio/collections/sets/ExercicioProposto01.java
@@ -0,0 +1,60 @@
+package src.com.dio.collections.sets;
+
+/*
+Crie uma conjunto contendo as cores do arco-íris e:
+a) Exiba todas as cores o arco-íris uma abaixo da outra;
+b) A quantidade de cores que o arco-íris tem;
+c) Exiba as cores em ordem alfabética;
+d) Exiba as cores na ordem inversa da que foi informada;
+e) Exiba todas as cores que começam com a letra ”v”;
+f) Remova todas as cores que não começam com a letra “v”;
+g) Limpe o conjunto;
+h) Confira se o conjunto está vazio;
+ */
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class ExercicioProposto01 {
+    public static void main(String[] args) {
+        System.out.println("Crie uma conjunto contendo as cores do arco-íris: ");
+        Set<String> coresArcoIris = new HashSet<>();
+        coresArcoIris.add("violeta");
+        coresArcoIris.add("anil");
+        coresArcoIris.add("azul");
+        coresArcoIris.add("verde");
+        coresArcoIris.add("amarelo");
+        coresArcoIris.add("laranja");
+        coresArcoIris.add("vermelho");
+        System.out.println(coresArcoIris);
+
+        System.out.println("Exiba todas as cores o arco-íris uma abaixo da outra: ");
+        coresArcoIris.forEach(System.out::println);
+
+        System.out.println("A quantidade de cores que o arco-íris tem: " + coresArcoIris.size());
+
+        System.out.println("Exiba as cores em ordem alfabética: ");
+        Set<String> coresArcoIris2 = new TreeSet<>(coresArcoIris);
+        System.out.println(coresArcoIris2);
+
+        System.out.println("Exiba as cores na ordem inversa da que foi informada ");
+        Set<String> coresArcoIris3 = new LinkedHashSet<>(
+                Arrays.asList("violeta", "anil", "azul", "verde", "amarelo", "laranja", "vermelho"));
+        System.out.println(coresArcoIris3);
+        List<String> coresArcoIrisList = new ArrayList<>(coresArcoIris3);
+        Collections.reverse(coresArcoIrisList);
+        System.out.println(coresArcoIrisList);
+
+        System.out.println("Exiba todas as cores que começam com a letra ”v”: ");
+        coresArcoIris.stream().filter(cor -> cor.startsWith("v")).forEach(System.out::println);
+
+        System.out.println("Remova todas as cores que não começam com a letra “v”: ");
+        coresArcoIris.removeAll(coresArcoIris.stream().filter(cor -> !cor.startsWith("v")).collect(Collectors.toSet()));
+        System.out.println(coresArcoIris);
+
+        coresArcoIris.clear();
+        System.out.println("Limpe o conjunto: " + coresArcoIris);
+
+        System.out.println("Confira se o conjunto está vazio: " + coresArcoIris.isEmpty());
+    }
+}
